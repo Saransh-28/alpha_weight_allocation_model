9a2bc0786443a2ca1c4baf54514bf106a9e895e8
Saransh Bhandari <83751767+Saransh-28@users.noreply.github.com>
2023-08-27 08:16:36 +0530

    Add files via upload

---
+++ data_creation.py
@@ -0,0 +1,99 @@
+# IMPORT ALL THE REQUIRED MODULE
+from alphas import *
+import alphas
+import re
+from helper_function import *
+import pandas as pd
+import numpy as np
+import yfinance as yf
+
+
+
+# DOWNLOAD ALL THE DATA 
+print('\nImport Done')
+start_date = '2013-01-01'
+end_date = '2020-07-12'
+keys = ['TSLA' , 'GOOG' , 'META' , 'AAPL' , 'MSFT' , 'AMZN' , 'NVDA' , 'V' , 'ORCL' , 'CSCO']
+open = pd.DataFrame(columns=keys)
+high =pd.DataFrame(columns=keys)
+low =pd.DataFrame(columns=keys)
+close = pd.DataFrame(columns=keys)
+volume = pd.DataFrame(columns=keys)
+vwap = pd.DataFrame(columns=keys)
+returns = pd.DataFrame(columns=keys)
+
+print('\nLoading the data\n')
+for s in keys:
+    temp = yf.download(s,
+                        start_date,
+                        end_date)
+    open[s] = temp['Open'].replace(np.nan ,0)
+    high[s] = temp['High'].replace(np.nan ,0)
+    low[s] = temp['Low'].replace(np.nan ,0)
+    close[s] = temp['Close'].replace(np.nan ,0)
+    volume[s] = temp['Volume'].replace(np.nan ,0)
+    vwap[s] = ((temp['High'] + temp['Low'] + temp['Close'])/3).replace(np.nan ,0)
+    returns[s] = temp.Close.pct_change().replace(np.nan ,0)
+
+print(' Printing the Stats\n')
+print('-'*20 + 'Data Shape' + '-'*20)
+
+open.fillna(0 , inplace=True)
+open.to_csv('data//base/open.csv', index=False)
+print('open -' , open.shape)
+
+high.fillna(0 , inplace=True)
+high.to_csv('data/base/high.csv', index=False)
+print('high -' , high.shape)
+
+low.fillna(0 , inplace=True)
+low.to_csv('data/base/low.csv', index=False)
+print('low -' , low.shape)
+
+close.fillna(0 , inplace=True)
+print('close -' , close.shape)
+close.to_csv('data/base/close.csv', index=False)
+
+volume.fillna(0 , inplace=True)
+print('volume -' , volume.shape)
+volume.to_csv('data/base/volume.csv', index=False)
+
+vwap.fillna(0 , inplace=True)
+print('vwap -' , vwap.shape)
+vwap.to_csv('data/base/vwap.csv', index=False)
+
+returns.fillna(0 , inplace=True)
+print('returns -' , returns.shape)
+returns.to_csv('data/base/returns.csv', index=False)
+
+print('-'*50)
+print('\nApplying the functions\n')
+
+
+
+# CREATING DATA FOR EACH COMPANY
+def apply_functions_and_sum(o ,h ,l , c , vwap ,v , r, function_list, function_dict , comp):
+    result_sum = pd.DataFrame()
+    for i , func in enumerate(function_list):
+        func_result = function_dict[func](o ,h ,l , c , vwap ,v , r)
+        func_result.to_csv(f'data/alphas/{str(func)}.csv' , index=True)
+        result_sum['A_'+str(i)] = func_result[comp]
+    return result_sum
+
+def natural_sort_key(s):
+    return [int(text) if text.isdigit() else text.lower() for text in re.split('([0-9]+)', s)]
+
+function_list = [i for i in dir(alphas) if 'alpha' in i]
+function_list = sorted(function_list, key=natural_sort_key)
+function_dict = {str(function_name): getattr(alphas, function_name) for function_name in function_list if callable(getattr(alphas, function_name))}
+print('All the available alphas')
+print(function_list)
+
+print('\nCreating Company Data\n')
+for col in list(open.columns):
+    name = col
+    temp = apply_functions_and_sum(open , high , low , close , vwap , volume , returns, function_list ,function_dict ,col)
+    temp.dropna(inplace=True)
+    temp.to_csv(f'data/company/{name}.csv',index=True)
+    print(f'Company {name} created -> {temp.shape} ')
+print('\nData Created\n')
---
+++ model.py
@@ -0,0 +1,68 @@
+import pandas as pd
+import numpy as np
+import tensorflow as tf
+from tensorflow import keras as k
+from tensorflow.keras import *
+from tensorflow.keras.models import *
+from tensorflow.keras.layers import *
+import seaborn as sns
+import warnings
+idx= pd.IndexSlice
+sns.set_style('whitegrid')
+warnings.filterwarnings('ignore')
+
+
+keys = ['TSLA' , 'GOOG' , 'META' , 'AAPL' , 'MSFT' , 'AMZN' , 'NVDA' , 'V' , 'ORCL' , 'CSCO']
+
+
+def get_model1(in_len,out_len):
+    main_input = tf.keras.layers.Input(shape=(in_len,))
+    
+    m = (Dense(512, activation='tanh')) (main_input)
+    m = (Dropout(0.2))(m)
+    m =(Dense(256, activation='tanh'))(m)
+    m = (Dropout(0.2))(m)
+    m =(Dense(128, activation='tanh')) (m)
+    m = (Dropout(0.2))(m)
+    m =(Dense(64, activation='tanh')) (m)
+    cnn_input = tf.keras.layers.Reshape((max_len, 1))(main_input)
+    cnn = tf.keras.layers.Conv1D(128, 2, padding='same', strides=1, activation='relu')(cnn_input)
+    cnn = tf.keras.layers.MaxPooling1D(pool_size=2)(cnn)
+
+    flat = tf.keras.layers.Flatten()(cnn)
+    flat = tf.keras.layers.Concatenate(axis=-1)([flat , m])
+    drop = tf.keras.layers.Dropout(0.2)(flat)
+    
+    dense1 = tf.keras.layers.Dense(512, activation='relu')(drop)
+    main_output = tf.keras.layers.Dense(out_len, activation='softmax')(dense1)
+    model = tf.keras.Model(inputs=main_input, outputs=main_output)
+    model.compile(loss='categorical_crossentropy', optimizer='rmsprop', metrics=['accuracy'])
+    return model
+# model1 = get_model1(X.shape[1])
+
+
+
+def get_model2(in_len,out_len):
+    x = Input(( in_len , out_len))
+    m = (Dense(512, activation='tanh')) (x)
+    m = (Dropout(0.2))(m)
+    m =(Dense(256, activation='tanh'))(m)
+    m = (Dropout(0.2))(m)
+    m =(Dense(128, activation='tanh')) (m)
+    m = (Dropout(0.2))(m)
+    m =(Dense(64, activation='tanh')) (m)
+    m =(Dense(128, activation='tanh'))(m)
+    m =(Dense(256, activation='tanh'))(m)
+    m = (Dense(512, activation='tanh'))(m)
+
+    t = tf.keras.layers.Concatenate(axis=-1)([x , m])
+    t =(Dense(256, activation='tanh'))(t)
+    f = (Dense(out_len, activation='softmax')) (t)
+
+    model = Model(inputs=x, outputs=f)
+    model.compile(loss='categorical_crossentropy', optimizer='rmsprop', metrics=['accuracy'])
+    return model
+
+#model2 = get_model2()
+
+
