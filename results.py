cdbf4ca613515519f65f6eab738dea5b73d0492c
Saransh <saranshbhandari28@gmail.com>
2023-09-14 09:03:10 +0530

    Add results.py to see the final result and make few changes in train_model.py

--- results.py
+++ results.py
@@ -1,19 +1,61 @@
 import os
 import pandas as pd
 import numpy as np
+import matplotlib.pyplot as plt
+from prettytable import PrettyTable
+
 res_list = (os.listdir('data/models/'))
 
-for res in res_list:
+headings = ['Company' , 'Buy and Hold' , 'Equal weights' , 'Using DL Model']
+values = []
+
+T = 100
+
+def calculate_returns(arr,  a):
+    prod = a
+    res = []
+    for i in arr:
+        res.append((1+i)*prod)
+        prod *= (1+i)
+    return prod , res
+
+for res in res_list:    
     weights = pd.read_csv('data/models/'+res)
     name = res.split('_')[0]
+    model_name = res.split('_')[1]
     alphas = pd.read_csv(f'data/company/{name}.csv')
-    results = pd.DataFrame()
-    print(weights.shape)
-    print(alphas.shape)
-    print(returns.shape)
-    # for i in range(weights.shape[1]):
-    #     print(weights.iloc[:,i])
-    #     print(alphas.iloc[:,i+1])
-    #     results[f'w_a_{i}'] = np.dot(alphas.iloc[:,i+1],weights.iloc[:,i])
-    # print(results.head())
-        
+    
+    
+    a_w = pd.DataFrame()
+    for col in weights.columns:
+        #            weights    *  alpha
+        a_w[col] = weights[col] * alphas[col[2:]] 
+    #  sum (weights*alpha)
+    s_a_w = a_w.sum(axis=1)
+    #  returns * sum (weights*alpha)
+    r_s_a_w = (s_a_w*alphas['returns'].shift(-1))[:-1]
+    r_s_a_w , r_s_a_w_arr = calculate_returns(r_s_a_w , T/10)
+    
+    
+    alp = list(weights.columns)
+    alp = [i[2:] for i in alp]
+    #  sum( alphas )
+    s_a = alphas[alp].sum(axis=1)
+    # returns * sum( alphas )
+    r_s_a = (s_a*alphas['returns'].shift(-1))[:-1]
+    r_s_a , r_s_a_arr = calculate_returns(r_s_a , T/100)
+    
+    x = list(alphas['close'])
+    b_h = (x[-1] - x[0])/x[0]
+    
+    values.append([name , b_h  , r_s_a , r_s_a_w])
+
+    plt.title(name + ' ' + model_name)
+    plt.plot(r_s_a_arr)
+    plt.plot(r_s_a_w_arr)
+    plt.legend(['equal weight' , 'Using DL Model'])    
+    plt.show()
+
+tab = PrettyTable(headings)
+tab.add_rows(values)
+print(tab)
--- train_model.py
+++ train_model.py
@@ -3,6 +3,7 @@ from model import *
 import re
 import os
 
+
 def natural_sort_key(s):
     return [int(text) if text.isdigit() else text.lower() for text in re.split('([0-9]+)', s)]
 
@@ -15,6 +16,10 @@ model_dict = {str(function_name): getattr(model, function_name) for function_nam
 def train_test(model , name, comp , X , y , test_size , frame_size ):
     train_size = int(frame_size*(1-test_size))
     val_df = pd.DataFrame(columns=y.columns)
+    print()
+    print('-'*30)
+    print()
+    print(f' STARTED TRAINING FOR {comp}')
     for i in range(0 , len(X) , frame_size):
         if (len(X) - i) <= frame_size:
             train_size = int((len(X) - i)*(1-test_size))
@@ -31,24 +36,45 @@ def train_test(model , name, comp , X , y , test_size , frame_size ):
         y_train = y_train.astype('float32') 
         X_test = X_test.astype('float32') 
         y_test = y_test.astype('float32') 
-        # print('training the model')
         model.fit(X_train , y_train, epochs=30 , verbose=0)
-        y_pred = pd.DataFrame(model.predict(X_test) , columns=y_test.columns)
-        # print(f'epoch {i+1} -> {model.evaluate(X_test , y_test)}')
+        y_pred = pd.DataFrame(model.predict(tempx) , columns=y_test.columns)
         val_df = pd.concat([val_df , y_pred] , axis=0)
+        
+    print(f' COMPLETED THE TRAINING FOR {comp} ')
     val_df.to_csv(f'./data/models/{comp}_{name}_result.csv',index=False)
+    print(f' SAVED THE RESULTS TO ./data/models/{comp}_{name}_result.csv')
+    print()
+    print('-'*30)
 
 
 comp_list = [i.split('.')[0] for i in os.listdir('data/company')]
 
+
+def softmax(df):
+    temp_df = []
+    for i in range(df.shape[0]):
+        a = df.iloc[i,:]
+        su = sum(a)
+        x = []
+        for i in a:
+            if su != 0:
+                x.append(i/su)
+            else:
+                x.append(0)
+        temp_df.append(x)
+    return pd.DataFrame(temp_df,columns=df.columns)
+
+
 def create_data(df):
     lis = list(df.columns)
     lis = [i for i in lis if 'A_' in i]
+    
     for i in lis:
-        df['y_'+i] = df[i].shift(-1)
+        df['y_'+i] = df[i]*(df['returns'].shift(-1))
+        df['y_'+i] = df['y_'+i].apply(lambda x: x if x > 0 else 0)
     lis = ['y_'+i for i in lis]
     df.dropna(inplace=True)
-    return df.drop(lis , axis=1) , df[lis]
+    return df.drop(lis , axis=1) , softmax(df[lis])
 
 model_list = [i for i in dir(model) if ('mod' == i[:3] and (i[-1]) in [str(i) for i in range(10)])]
 model_list = sorted(model_list, key=natural_sort_key)
@@ -57,11 +83,12 @@ model_dict = {str(function_name): getattr(model, function_name) for function_nam
 for comp in comp_list:
     X , y = create_data(pd.read_csv('data/company/'+comp+'.csv'))
     print(comp)
-    # print(model_list)
+    print(X.head())
+    print(y.head())
+    print(model_list)
     for model in model_list:
-        print('_________')
+        print('*'*30)
         print(model)
         x = model_dict[model](X.shape[1]-1 , y.shape[1])
-        # print(x.summary())
         train_test(x , model , comp , X , y , test_size=0.2 , frame_size=30 )
-        print('_________')
+        print('*'*30)
